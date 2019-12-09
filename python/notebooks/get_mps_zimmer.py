3f2ab2741a0cdfc99ff16c82a7d22d85d6df4a72
Amin <amin-nejat@bitbucket.org>
2019-12-09 13:58:58 -0500

    Updated Zimmer GMM script

--- python/notebooks/get_mps_zimmer.py
+++ python/notebooks/get_mps_zimmer.py
@@ -1,20 +1,19 @@
 """Extract MP components from Zimmer data and write to MAT file"""
 
+import argparse
+
 import h5py
 import numpy as np
 from skimage.util import img_as_float
 from scipy.io import savemat
 
+from readers import ZimmerReader
 from imagerep import mp_gaussian
 
 
 # Input and output paths
-IN_FPATH = '/home/mn2822/Desktop/WormTracking/data/zimmer/mCherry_v00065-01581.hdf5'
-OUT_FPATH = '/home/mn2822/Desktop/WormTracking/data/zimmer/sample_video/mat/vid_0650_0700.mat'
-
-# Start and stop times for extraction
-T_START = 650
-T_STOP = 700
+IN_FPATH = '/home/mn2822/Desktop/WormOT/data/zimmer/mCherry_v00065-01581.hdf5'
+OUT_DIR = '/home/mn2822/Desktop/WormOT/data/zimmer/all_gmms'
 
 # Covariance values for each dimension
 COV_DIAG = [4.0, 4.0, 1.0]
@@ -23,37 +22,56 @@ COV_DIAG = [4.0, 4.0, 1.0]
 N_ITER = 500
 
 
-def main():
+def parse_args():
 
-    cov = np.diag(COV_DIAG)
+    parser = argparse.ArgumentParser(description='Compute GMMs for Zimmer data')
+    parser.add_argument('--start', '-s', type=int, help='frame to start at')
+    parser.add_argument('--end', '-e', type=int, help='frame to end at')
+
+    return parser.parse_args()
+
+
+def get_gmms(fpath, t_start, t_stop, cov, n_iter):
+
     means = []
     weights = []
 
-    with h5py.File(IN_FPATH, 'r') as f:
+    with ZimmerReader(fpath) as rdr:
 
-        dset = f.get('mCherry')
-
-        for t in range(T_START, T_STOP):
+        for t in range(t_start, t_stop):
 
             print(f'Frame: {t}')
 
             # Load frame
-            img_raw = dset[t, 0, :, :, :]
-            img_raw = np.moveaxis(img_raw, [0, 1, 2], [2, 1, 0])
-            img = img_as_float(img_raw)
+            img = rdr.get_frame(t)
 
             # Extract MP components from frame
-            mus, wts, _ = mp_gaussian(img, cov, N_ITER)
+            mus, wts, _ = mp_gaussian(img, cov, n_iter)
             means.append(mus)
             weights.append(wts)
 
+    return (means, weights)
+
+
+def main():
+
+    # Parse arguments
+    args = parse_args()
+    t_start = args.start
+    t_stop = args.end
+
+    # Compute GMMs
+    cov = np.diag(COV_DIAG)
+    means, weights = get_gmms(IN_FPATH, t_start, t_stop, cov, N_ITER)
+
     # Write means, weights, and covariance to MAT file
+    out_fpath = f'{OUT_DIR}/vid_{t_start:04}_{t_stop:04}.mat'
     mat_dict = {
         'means': np.array(means),
         'weights': np.array(weights),
         'cov': cov
     }
-    savemat(OUT_FPATH, mat_dict)
+    savemat(out_fpath, mat_dict)
     
     
 if __name__ == '__main__':
