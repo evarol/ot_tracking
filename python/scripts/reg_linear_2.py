aa371f39ae1d7bb13c1bc788028142b6ab37cb8a
Conor McGrory <cpmcgrory@gmail.com>
2020-02-25 14:56:10 -0500

    Wrote second registration script

---
+++ python/scripts/reg_linear_2.py
@@ -0,0 +1,69 @@
+"""Script for computing linear transforms from all frames to frame 1"""
+
+import numpy as np
+from scipy.io import loadmat, savemat
+
+from otimage.imagereg import ot_reg_linear
+
+
+MP_FPATH = '/home/mn2822/Desktop/WormOT/data/zimmer/mp_components/mp_0000_0050.mat'
+OUT_FPATH = '/home/mn2822/Desktop/WormOT/data/zimmer/registration/reg_2_0000_0050.mat'
+N_FRAMES = 50
+N_MPS = 100
+
+
+def main():
+
+    print(f'Loading MP data from {MP_FPATH}...')
+
+    mp_data = loadmat(MP_FPATH)
+    pts = mp_data['means']
+    wts = mp_data['weights']
+    cov = mp_data['cov']
+
+    print('Computing transformations...')
+
+    # Points and weights for first frame
+    pts_0 = pts[0, 0:N_MPS, :]
+    wts_0 = wts[0, 0:N_MPS, 0]
+    
+    f_alpha = [None] * N_FRAMES
+    f_beta = [None] * N_FRAMES
+    rec_pts = [None] * N_FRAMES
+
+    f_alpha[0] = np.array([0, 0, 0])
+    f_beta[0] = np.eye(3)
+    rec_pts[0] = pts_0
+
+    for t in range(1, N_FRAMES):
+
+        print(f'frame: {t}')
+
+        # Points and weight distribution for current frame
+        pts_t = pts[t, 0:N_MPS, :]
+        wts_t = wts[t, 0:N_MPS, 0]
+
+        # Compute mapping from reconstruction of prev. frame to current frame
+        alpha, beta, _ = ot_reg_linear(rec_pts[t - 1], pts_t, wts_0, wts_t)
+
+        # Compute mapping from first frame to current frame using recursive update
+        f_alpha[t] = beta @ f_alpha[t - 1] + alpha
+        f_beta[t] = beta @ f_beta[t - 1]
+
+        # Use mapping to reconstruct current frame from first frame
+        rec_pts[t] = f_alpha[t] + f_beta[t] @ pts_0
+
+    print(f'Saving results to {OUT_FPATH}')
+    mat_dict = {
+        'fname': MP_FPATH,
+        'n_frames': N_FRAMES,
+        'n_mps': N_MPS,
+        'f_alpha': np.array(f_alpha),
+        'f_beta': np.array(f_beta),
+        'rec_pts': np.array(rec_pts),
+    }
+    savemat(OUT_FPATH, mat_dict)
+
+
+if __name__ == '__main__':
+    main()
