f5cafa2298ac8afb9f57922e4660d2ab83b6130b
Amin <amin-nejat@github.com>
2020-04-21 01:29:07 -0400

    Added notebook for running registration on Vivek data

--- python/scripts/mp_vivek.sh
+++ python/scripts/mp_vivek.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python compute_mps.py \
-    -i /home/mn2822/Desktop/WormTraces/data/Vivek/0930_tail_01/data.mat \
-    -o /home/mn2822/Desktop/WormOT/data/vivek/0930_tail_01/mp_components/mp_0000_0900.mat \
+    -i /home/mn2822/Desktop/WormTraces/data/Vivek/1024_tail_06/data.mat \
+    -o /home/mn2822/Desktop/WormOT/data/vivek/1024_tail_06/mp_components/mp_0000_0900.mat \
     -d 'vivek' \
     -p 12 \
     -n 400 \
